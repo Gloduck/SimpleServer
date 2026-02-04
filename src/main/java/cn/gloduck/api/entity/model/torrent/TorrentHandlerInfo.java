3ea8edf59b9bd62b7ddaf9945bd0e62e620217d6
xianting.ke <xianting.ke@zstack.io>
2026-02-04 10:42:19 +0800

    feat: Torrent排序支持通过+、-指定升降序

--- src/main/java/cn/gloduck/api/entity/model/torrent/TorrentHandlerInfo.java
+++ src/main/java/cn/gloduck/api/entity/model/torrent/TorrentHandlerInfo.java
@@ -14,5 +14,10 @@ public class TorrentHandlerInfo {
 
     private List<String> tags;
 
+    /**
+     * 支持的排序字段
+     * 格式: "+field" = 只支持升序, "-field" = 只支持降序, "field" = 支持两种排序
+     * 返回空列表表示不支持排序
+     */
     private List<String> supportSortFields;
 }
