f7efea89bacc2d610ecf8fda906fee17ba0ba340
Gloduck <qq1035311527@163.com>
2026-02-05 13:51:38 +0800

    opt: 优化torrent代码结构，抽取方法

--- src/main/java/cn/gloduck/api/service/torrent/handler/AbstractTorrentHandler.java
+++ src/main/java/cn/gloduck/api/service/torrent/handler/AbstractTorrentHandler.java
@@ -2,22 +2,15 @@ package cn.gloduck.api.service.torrent.handler;
 
 import cn.gloduck.api.entity.config.TorrentConfig;
 import cn.gloduck.api.exceptions.ApiException;
-import cn.gloduck.api.utils.NetUtils;
+import cn.gloduck.api.utils.HttpClientUtils;
 import cn.gloduck.server.core.util.JsonUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -60,43 +53,6 @@ public abstract class AbstractTorrentHandler implements TorrentHandler {
         } catch (IOException | InterruptedException e) {
             return false;
         }
-    }
-
-    private HttpClient buildClient(TorrentConfig.WebConfig config) {
-        HttpClient.Builder builder = HttpClient.newBuilder();
-        Integer timeout = Optional.ofNullable(config.connectTimeout).orElse(5);
-        builder.connectTimeout(java.time.Duration.ofSeconds(timeout));
-        InetSocketAddress proxyAddress = NetUtils.buildProxyAddress(config.proxy);
-        if (proxyAddress != null) {
-            builder.proxy(java.net.ProxySelector.of(proxyAddress));
-        }
-        Boolean trustAllCertificates = Optional.ofNullable(config.trustAllCertificates).orElse(false);
-        if (trustAllCertificates) {
-            SSLContext sslContext = null;
-            try {
-                sslContext = SSLContext.getInstance("TLS");
-                sslContext.init(null, new TrustManager[]{
-                        new X509TrustManager() {
-                            @Override
-                            public X509Certificate[] getAcceptedIssuers() {
-                                return null;
-                            }
-
-                            @Override
-                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                            }
-
-                            @Override
-                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                            }
-                        }
-                }, new java.security.SecureRandom());
-                builder.sslContext(sslContext);
-            } catch (NoSuchAlgorithmException | KeyManagementException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return builder.build();
     }
 
     public JsonNode sendJsonRequest(HttpRequest request) {
@@ -146,7 +102,7 @@ public abstract class AbstractTorrentHandler implements TorrentHandler {
         this.baseUrl = config.url;
         this.requestTimeout = Optional.ofNullable(config.requestTimeout).orElse(5);
         this.validStatusTimeout = Optional.ofNullable(config.validStatusTimeout).orElse(1);
-        this.httpClient = buildClient(config);
+        this.httpClient = HttpClientUtils.buildClient(config.connectTimeout, config.proxy, Boolean.TRUE.equals(config.trustAllCertificates));
     }
 
 }
