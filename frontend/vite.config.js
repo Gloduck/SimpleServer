a35a13ab7fd28ba8525671e16e15ebf87b6ce5f8
Gloduck <qq1035311527@163.com>
2026-03-19 16:26:08 +0800

    opt：去掉vue自带的打包配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,9 +9,5 @@ export default defineConfig(({ command }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       vue: 'vue/dist/vue.esm-bundler.js'
     }
-  },
-  build: {
-    outDir: '../backend/src/main/resources/front',
-    emptyOutDir: true
   }
 }));
