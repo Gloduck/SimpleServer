f230f672932780489877f713cc90fc28f81b4f37
Gloduck <qq1035311527@163.com>
2026-03-20 11:51:44 +0800

    opt: 优化数据库读取位置

--- backend/src/main/java/cn/gloduck/dao/CsvDbFactoryPool.java
+++ backend/src/main/java/cn/gloduck/dao/CsvDbFactoryPool.java
@@ -1,9 +1,13 @@
 package cn.gloduck.dao;
 
 
+import cn.gloduck.api.ApplicationContext;
 import cn.gloduck.db.CsvDbFactory;
 import cn.gloduck.db.converter.CsvJsonBasedConverter;
 
 public class CsvDbFactoryPool {
-    public static final CsvDbFactory clipboard = new CsvDbFactory("./db/clipboard", new CsvJsonBasedConverter());
+    public static final CsvDbFactory clipboard = new CsvDbFactory(
+            ApplicationContext.resolveApplicationDirectory().resolve("db").resolve("clipboard").toString(),
+            new CsvJsonBasedConverter()
+    );
 }
